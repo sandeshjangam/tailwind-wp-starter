6c9abd602541bb8f68f6c72937d6e49108b94187
Sandesh <sandeshj@bsf.io>
2021-12-01 15:55:34 +0530

    Tailwind css added

--- build/app.asset.php
+++ build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '719c6f4d34a2bea37ad919b53791de79');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '57306b6ed8b310c5624f53001105ac53');
--- src/Settings.js
+++ src/Settings.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Settings() {
 
-	return <h1>Hello Sandesh Here</h1>;
+	return <h1 className="text-red-500">Hello Sandesh Here</h1>;
 }
 
 export default Settings;
